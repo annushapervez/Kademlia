70eb2b0bbdb70259d02d19e2665ac0eb2324e1a8
Annusha Pervez <annushapervez7@gmail.com>
2024-10-28 20:40:26 -0400

    Create README.go

---
+++ README.go
@@ -0,0 +1,12 @@
+// README.go
+package main
+
+import "fmt"
+
+// Main function that highlights the project's status
+func main() {
+    fmt.Println("This repository contains code related to an academic project.")
+    fmt.Println("Due to academic integrity policies, the actual project cannot be uploaded.")
+    fmt.Println("This GitHub repository is created to maintain a portfolio and demonstrate my work.")
+    fmt.Println("Please contact me for further information regarding this project.")
+}
